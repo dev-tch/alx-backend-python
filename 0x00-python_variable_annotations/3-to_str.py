e663f762a9649c836e26b74c401a1bb1afddf2e8
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-19 15:24:17 +0000

    task3- to string function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" module with one function to_str """
+
+
+def to_str(n: float) -> str:
+    """ function to for string representation of float"""
+    return "{:.2f}".format(n)
