08c65ce35f145a0286ca533911b48dda9205f71f
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-19 15:02:07 +0000

    task1: function concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" module with one function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function to concat two strings"""
+    return "{}{}".format(str1, str2)
