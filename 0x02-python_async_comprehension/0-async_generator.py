0fbf55f7b1f979ab7739681efe242d85fdaef053
dev-tch <tijeni.chaabeni@gmail.com>
2024-04-23 22:00:44 +0000

    task0: async geneartor

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ module with one coroutine async_generator """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ loop 10 times, each time asynchronously wait 1 second"""
     for _ in range(10):
         await asyncio.sleep(1)
