5d525704649cf939722259cb818bbba0e1021bc4
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-12 03:00:13 +0000

    test32

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,8 +10,8 @@ class TestAccessNestedMap (unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-        ])
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
     def test_access_nested_map(self, nested_map, path, expected):
         """ test case for method access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
