71c578ebe2ca21a7b30b399d30d9d022db631e0a
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-12 00:46:12 +0000

    fix requirement

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,10 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from typing import (
+    Mapping,
+    Sequence,
+)
 
 
 class TestAccessNestedMap (unittest.TestCase):
@@ -12,6 +16,7 @@ class TestAccessNestedMap (unittest.TestCase):
         ("test_02", {"a": {"b": 2}}, ("a",), {'b': 2}),
         ("test_03", {"a": {"b": 2}}, ("a", "b"), 2),
         ])
-    def test_access_nested_map(self, _, a, b, expected):
+    def test_access_nested_map(self, _, a: Mapping,
+                               b: Sequence, expected: bool) -> bool:
         """ test case for method access_nested_map"""
         self.assertEqual(access_nested_map(a, b), expected)
