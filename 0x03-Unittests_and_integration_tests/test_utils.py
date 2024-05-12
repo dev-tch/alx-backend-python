84e4a5812adf23bfa8c5355bd4a63e6ab963343f
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-12 00:53:52 +0000

    test method test_access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,19 +4,20 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 from typing import (
-    Mapping,
-    Sequence,
+    Dict,
+    Tuple,
+    Union
 )
 
 
 class TestAccessNestedMap (unittest.TestCase):
     """ implement test class"""
     @parameterized.expand([
-        ("test_01", {"a": 1}, ("a",), 1),
-        ("test_02", {"a": {"b": 2}}, ("a",), {'b': 2}),
-        ("test_03", {"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
         ])
-    def test_access_nested_map(self, _, a: Mapping,
-                               b: Sequence, expected: bool) -> bool:
+    def test_access_nested_map(self, a: Dict, b: Tuple[str],
+                               expected: Union[Dict, int]) -> None:
         """ test case for method access_nested_map"""
         self.assertEqual(access_nested_map(a, b), expected)
