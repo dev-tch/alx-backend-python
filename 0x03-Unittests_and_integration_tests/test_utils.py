97c7aa9982c63ba8db794c59bbf87f936f9d21b8
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-12 00:56:53 +0000

    test task0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,7 @@ class TestAccessNestedMap (unittest.TestCase):
     """ implement test class"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
         ])
     def test_access_nested_map(self, a: Dict, b: Tuple[str],
