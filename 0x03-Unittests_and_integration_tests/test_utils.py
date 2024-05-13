3f9e69b24f7e2b17754720c34a70a4519bbd4924
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-13 03:10:48 +0000

    test memorization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -40,3 +40,25 @@ class TestGetJson(unittest.TestCase):
         with patch('requests.get', return_value=Mock(**attrs)) as mock_request:
             self.assertEquals(get_json(test_url), test_payload)
             mock_request.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """ implement class TestMemoize"""
+    def test_memoize(self):
+        """ Parameterize and patch"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method',
+                          return_value=lambda: 42) as mock_method:
+            instance = TestClass()
+            instance.a_property()
+            self.assertEquals(instance.a_property(), 42)
+            self.assertEquals(instance.a_property(), 42)
+            mock_method.assert_called_once()
